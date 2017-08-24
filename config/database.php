c2ac90d37c378ddf9e54df3db6282a68de5159a9
qskane <qskane@qq.com>
2017-08-24 14:57:02 +0800

    config

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', ''),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'eopark'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'ASDF;lkj1234'),
+            'database' => env('DB_DATABASE', ''),
+            'username' => env('DB_USERNAME', ''),
+            'password' => env('DB_PASSWORD', ''),
 //            'password' => env('DB_PASSWORD', 'root'),//
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
